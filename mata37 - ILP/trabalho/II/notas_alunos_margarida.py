91c5f365b1688c1e0208e63b7b4c6cdb3a5ac89a
Margarida <margaridammcm@gmail.com>
2022-11-06 17:26:36 -0300

    init project

--- mata37 - ILP/trabalho/II/notas_alunos_margarida.py
+++ mata37 - ILP/trabalho/II/notas_alunos_margarida.py
@@ -1,17 +1,61 @@
 import csv
 
-with open('gabarito.csv') as f:
+def dict_gabarito():
+    with open('gabarito.csv') as f:
+        DictReader_obj = csv.DictReader(f)
+        for resposta in DictReader_obj:
+            return resposta
+print('DICIONÁRIO DO GABARITO: ', dict_gabarito())
+dict_gabarito = dict_gabarito()
 
-    DictReader_obj = csv.DictReader(f)
 
-    for item in DictReader_obj:
+def lista_dicts_respostas():
+    lista_dicts_respostas = []
+    with open('respostas.csv') as f:
+        DictReader_obj = csv.DictReader(f)
+        for resposta in DictReader_obj:
+            lista_dicts_respostas.append((resposta))
+    return lista_dicts_respostas
+print('LISTA DE DICIONÁRIOS COM RESPOSTAS: ',lista_dicts_respostas())
+lista_dicts_respostas = lista_dicts_respostas()
 
-        print(dict(item))
 
-with open('respostas.csv') as f:
+def lista_respostas_questao1():
+    lista_respostas_questao1 = [resposta['resposta1'] for resposta in lista_dicts_respostas]
+    return lista_respostas_questao1
+print('LISTA DE RESPOSTAS DA QUESTÃO 1: ',lista_respostas_questao1())
+lista_respostas_questao1 = lista_respostas_questao1()
 
-    DictReader_obj = csv.DictReader(f)
 
-    for item in DictReader_obj:
+def checa_acertos_questao1():
+    i = 0
+    while i < len(lista_respostas_questao1):
+        if lista_respostas_questao1[i] == dict_gabarito['resposta1']:
+            lista_respostas_questao1[i] = 1
+        else:
+            lista_respostas_questao1[i] = 0
+        i += 1
+    return lista_respostas_questao1
+print('LISTA DE ACERTOS DA QUESTÃO 1:', checa_acertos_questao1())
+checa_acertos_questao1 = checa_acertos_questao1()
 
-        print(dict(item))
+
+def lista_respostas_questao2():
+    lista_respostas_questao2 = [resposta['resposta2'] for resposta in lista_dicts_respostas]
+    return lista_respostas_questao2
+print('LISTA DE RESPOSTAS DA QUESTÃO 2: ',lista_respostas_questao2())
+lista_respostas_questao2 = lista_respostas_questao2()
+
+
+def checa_acertos_questao2():
+    i = 0
+    while i < len(lista_respostas_questao2):
+        if lista_respostas_questao2[i] == dict_gabarito['resposta2']:
+            lista_respostas_questao2[i] = 1
+        else:
+            lista_respostas_questao2[i] = 0
+        i += 1
+    return lista_respostas_questao2
+print('LISTA DE ACERTOS DA QUESTÃO 2:', checa_acertos_questao2())
+checa_acertos_questao2 = checa_acertos_questao2()
+
