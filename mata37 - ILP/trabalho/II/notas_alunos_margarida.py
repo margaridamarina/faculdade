f641eca7c1da95cacac71871013eb3fb6f8b1d60
Margarida <margaridammcm@gmail.com>
2022-11-15 11:20:25 -0300

    finaliza dados

--- mata37 - ILP/trabalho/II/notas_alunos_margarida.py
+++ mata37 - ILP/trabalho/II/notas_alunos_margarida.py
@@ -7,9 +7,16 @@ def matriz_gabarito():
         for row in reader:
             gabarito_junto = ''.join(row)
             matriz_gab.append(gabarito_junto.split(';'))
-        print(matriz_gab)
+        # print('A matriz do gabarito é: ', matriz_gab)
         return matriz_gab
-gabarito = matriz_gabarito()
+
+
+def lista_gabarito(matriz_gab):
+    lista_gabarito = []
+    for questao in matriz_gab:
+        lista_gabarito.append(questao[1])
+    # print('As respostas corretas são: ', lista_gabarito)
+    return lista_gabarito
 
 
 def matriz_respostas():
@@ -18,64 +25,56 @@ def matriz_respostas():
         reader = csv.reader(f)
         for row in reader:
             respostas_junto = ''.join(row)
-            matriz_resp.append(respostas_junto.split(';'))
-        print(matriz_resp)
+            matriz_resp.append(respostas_junto.split('; '))
+        # print('A matriz das respostas dos alunos é:', matriz_resp)
         return matriz_resp
-respostas = matriz_respostas()
 
 
-def lista_gabarito(matriz_gab):
-    lista_gabarito = []
-    for questao in matriz_gab:
-        lista_gabarito.append(questao[1])
-    print(lista_gabarito)
-    return lista_gabarito
-lista_gabarito = lista_gabarito(gabarito)
+def lista_respostas(matriz_resp):
+    lista_respostas_alunos = []
+    for lin in range(len(matriz_resp)):
+        for col in range(5,25,2):
+            respostas = matriz_resp[lin][col]
+            lista_respostas_alunos.append(respostas)
+    # print('As respostas foram: ', lista_respostas_alunos)
+    return lista_respostas_alunos
 
+
+def gerador_listas_respostas_alunos_separadas(lista_respostas_alunos, numero_questoes):
+    for i in range(0, len(lista_respostas_alunos), numero_questoes):
+        yield lista_respostas_alunos[i:i + numero_questoes]
+
+
+def compara_respostas(lista_a_ser_conferida, lista_gab):  
+    comparacao = [i for i, j in zip(lista_a_ser_conferida, lista_gab) if i == j]
+    qtd_acertos = len(comparacao)
+    # print('Os matches são: ', comparacao)
+    # print('A quantidade de acertos foi:', qtd_acertos)
+    return qtd_acertos
 
 def lista_matriculas(matriz_resp):
     lista_matriculas = []
     for resposta in matriz_resp:
         lista_matriculas.append(resposta[1])
-    print(lista_matriculas)
+    print('A lista das matrículas é: ', lista_matriculas)
     return lista_matriculas
-lista_matriculas = lista_matriculas(respostas)
+lista_matriculas(matriz_respostas())
 
 
 def lista_nomes(matriz_resp):
     lista_nomes = []
     for resposta in matriz_resp:
         lista_nomes.append(resposta[3])
-    print(lista_nomes)
+    print('A lista com os nomes dos alunos é: ', lista_nomes)
     return lista_nomes
-lista_nomes = lista_nomes(respostas)
+lista_nomes(matriz_respostas())
 
 
-def lista_respostas(matriz_resp):
-    lista_respostas = []
-    for resposta in matriz_resp:
-        lista_respostas.append(resposta[5,7,9,11,13,15,17,19,21,23])
-    print(lista_respostas)
-    return lista_respostas
-lista_respostas = lista_respostas(respostas)
-
-
-# def lista_respostas_questao7():
-#     lista_respostas_questao7 = [resposta['resposta7'] for resposta in lista_dicts_respostas]
-#     return lista_respostas_questao7
-# print('LISTA DE RESPOSTAS DA QUESTÃO 7: ',lista_respostas_questao7())
-# lista_respostas_questao7 = lista_respostas_questao7()
-
-
-# def checa_acertos_questao7():
-#     i = 0
-#     while i < len(lista_respostas_questao7):
-#         if lista_respostas_questao7[i] == dict_gabarito['resposta7']:
-#             lista_respostas_questao7[i] = 1
-#         else:
-#             lista_respostas_questao7[i] = 0
-#         i += 1
-#     return lista_respostas_questao7
-# print('LISTA DE ACERTOS DA QUESTÃO 7:', checa_acertos_questao7())
-# checa_acertos_questao7 = checa_acertos_questao7()
-
+def lista_acertos(matriz_respostas_sep):
+    lista_acertos = []
+    for resposta in matriz_respostas_sep:
+        # print('As respostas separadas do aluno foi:', resposta)
+        lista_acertos.append(compara_respostas(resposta, lista_gabarito(matriz_gabarito())))
+    print('A lista de acertos foi:', lista_acertos)
+    return lista_acertos
+lista_acertos(gerador_listas_respostas_alunos_separadas(lista_respostas(matriz_respostas()), 10))    
